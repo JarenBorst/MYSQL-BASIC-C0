09dda888b326cdc55e51663a7b8081289300c8ab
Jaren Borst <jarenborst@student.rocva.nl>
2022-01-27 10:50:08 +0100

    opdracht 7 af

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,6 @@
 -- Opdracht 1
-
+SELECT * FROM players WHERE id="198329"
 -- Opdracht 2
-
+DELETE FROM players WHERE id = 198329
 -- Opdracht 3
+-- het toevoegen van een primary key aan een id is gelukt
